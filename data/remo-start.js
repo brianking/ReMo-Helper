f51095c694f0ba1a213db6577a930f9fafe413b2
brianking <brian@kingsonline.net>
2012-02-14 13:23:34 +0100

    First run at first-run

---
+++ data/remo-start.js
@@ -0,0 +1,6 @@
+document.addEventListener("RemoOpenAddonsManager", function(e) { self.postMessage("openaddons") }, false, true);
+
+self.port.on('version', function(version)
+{
+    document.getElementById("version").textContent = 'v' + version;
+});
