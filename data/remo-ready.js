b93cc3e661541112daf7d5b6af4c82fb469e6dc3
Erik Vold <erikvvold@gmail.com>
2012-06-07 20:33:48 -0700

    Closes #11 adding about:reps (w/ some hacks)

---
+++ data/remo-ready.js
@@ -0,0 +1,10 @@
+/**
+ * Actions for remo-start page
+ */
+document.addEventListener("RemoOpenAddonsManager", function (e) { 
+                            self.postMessage("openaddons") 
+                          }, false, true);
+
+self.port.on('version', function (version) {
+  document.getElementById("version").textContent = 'v' + version;
+});
--- data/remo-start.js
+++ data/remo-start.js
@@ -1,10 +1,29 @@
-/**
- * Actions for remo-start page
- */
-document.addEventListener("RemoOpenAddonsManager", function (e) { 
-                            self.postMessage("openaddons") 
-                          }, false, true);
+"use strict";
 
-self.port.on('version', function (version) {
-  document.getElementById("version").textContent = 'v' + version;
+let baseURLEle = document.createElement("base");
+baseURLEle.setAttribute("href", "");
+
+let link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+
+let baseURL;
+
+self.port.on('baseURL', function (url) {
+  baseURL = url;
+  baseURLEle.setAttribute("href", url);
+  link.setAttribute("href", url+"remo-start.css");
 });
+
+document.addEventListener("StartAboutReps", function (e) {
+  document.getElementsByTagName("head")[0].appendChild(baseURLEle);
+  document.getElementsByTagName("head")[0].appendChild(link);
+
+  window.addEventListener("load", function() {
+    var imgs = document.getElementsByTagName("img");
+    Array.prototype.slice.call(imgs).forEach(function(img) {
+      var src = img.getAttribute("src");
+      if (src.match(/:/)) return;
+      img.setAttribute("src", baseURL+src);
+    });
+  }, false);
+}, false, true);
